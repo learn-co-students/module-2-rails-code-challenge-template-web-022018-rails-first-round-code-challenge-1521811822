ccf59d2b406fa20459c6c57bb8299068d7f8d5fc
dannyb21892 <dannyb21892@verizon.net>
2018-03-23 10:32:56 -0400

    :)

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,31 @@
 class SuperheroesController < ApplicationController
   def index
-    @superheroes = Superhero.all
+    @superpowers = Superpower.all
+    if params[:search]
+      @superheroes = Superhero.all.select{|sh| sh.superpower_id.to_s == params[:search][:superpower_id]}
+      @search_flag = true
+    else
+      @superheroes = Superhero.all
+    end
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+    redirect_to superheroes_path
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
   end
 end
--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,2 +1,4 @@
 class Superpower < ApplicationRecord
+  has_many :superheroes
+  validates :name, :description, presence: true
 end
