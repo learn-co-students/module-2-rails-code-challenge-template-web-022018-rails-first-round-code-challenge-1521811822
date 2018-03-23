38f94fdc24a9c6b414e260b041655c34a45bb524
alec-petros <a.petros@verizon.net>
2018-03-23 10:01:49 -0400

    Completed requirements

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,41 @@
 class SuperheroesController < ApplicationController
+
+  before_action :set_superhero, only: [:show]
+
   def index
-    @superheroes = Superhero.all
+    @superpowers = Superpower.all
+    if params[:superpower_id]
+      @superheroes = Superhero.where(superpower_id: params[:superpower_id])
+    else
+      @superheroes = Superhero.all
+    end
   end
+
+  def show
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    if @superhero.valid?
+      @superhero.save
+      redirect_to @superhero
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,18 @@
 class SuperpowersController < ApplicationController
+
+  before_action :set_superpower, only: [:show]
+
   def index
     @superpowers = Superpower.all
+  end
+
+  def show
+    @heroes = Superhero.where(superpower: @superpower)
+  end
+
+  private
+
+  def set_superpower
+    @superpower = Superpower.find(params[:id])
   end
 end
