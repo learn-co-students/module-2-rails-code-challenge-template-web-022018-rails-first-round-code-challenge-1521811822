d32882cc09b519b5bca3f7ea0fdee4cd80048bee
github username <github email address>
2018-03-23 10:41:11 -0400

    Done.

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,44 @@
 class SuperheroesController < ApplicationController
+
   def index
-    @superheroes = Superhero.all
+    if params[:superpower]
+      @superheroes = Superhero.select {|hero| hero.superpower.name.downcase.include?(params[:superpower].downcase)}
+    else
+      @superheroes = Superhero.all
+    end
   end
+
+  def show
+    @superhero= Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+    @superpowers = Superpower.all
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+      if @superhero.valid?
+        @superhero.save
+        redirect_to superhero_path(@superhero)
+      else
+        flash[:error] = @superhero.errors.full_messages
+        redirect_to new_superhero_path
+      end
+  end
+
+  def search
+    @superpower = params[:superpower]
+    @superheroes = Superhero.all.select do |superhero|
+      superhero.superpower.name == params[:superpower]
+    end
+  end
+
+  private
+
+    def superhero_params
+      params.require(:superhero).permit(:name, :super_name, :superpower_id)
+    end
+
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,7 @@
 class Superhero < ApplicationRecord
+
+validates :super_name, :name, :superpower_id, presence: true
+validates :super_name, uniqueness: true
+belongs_to :superpower
+
 end
