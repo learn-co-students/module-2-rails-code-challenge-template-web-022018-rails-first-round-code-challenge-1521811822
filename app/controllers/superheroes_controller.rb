8e5ab775dc62c5f0d4897f95f8eb29dd7e962590
zafinar <zafinar@gmail.com>
2018-03-23 10:45:50 -0400

    Done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,30 @@
 class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
+    if params[:search]
+    @superheroes = Superhero.superpower.select(params[:search])
+    redirect_to @superheroes
+    end
+
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(hero_params)
+    @superhero.save
+    redirect_to @superhero
+  end
+
+  private
+  def hero_params
+    params.require(:superhero).permit(:name,:super_name,:superpower_id)
+  end
+
 end
