eac17dd9708f3e3551bbd634d88f97706fa68dce
github username <github email address>
2018-03-23 10:45:50 -0400

    done

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,27 @@
 class SuperpowersController < ApplicationController
   def index
+
     @superpowers = Superpower.all
+
+    # SEARCH STUFF ========================
+
+    if params[:search]
+      @superpower = Superpower.search(params[:search]).order("created_at DESC")
+    else
+      @superpower = Superpower.all.order("created_at DESC")
+    end
+
+      # SEARCH STUFF ========================
+
+
   end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+  private
+    def superpower_params
+      params.require(:superpower).permit(:name, :description)
+    end
 end
--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,2 +1,10 @@
 class Superpower < ApplicationRecord
+  has_many :superheros
+
+
+# SEARCH STUFF ======================
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+  end
+
 end
